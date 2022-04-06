0acb3aa7248a3d4fbfe3ffb9c2eb4b1f6233c22c
Daniel <danielkhenriksen@gmail.com>
2022-04-06 18:10:24 +0200

    Stored previous configs for 5x5 and 7x7

--- Assignment 2/misc/config_5x5.py.py
+++ Assignment 2/misc/config_5x5.py.py
@@ -1,62 +1,65 @@
 import logging
 from functools import reduce
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
 
 
 class App:
-    _conf = {                                               # --- MAIN CONFIGS ---
+    _conf = {                                               # === MAIN CONFIGS ===
         "run": [],                                          # Main tasks [ RL | TOPP | OHT ]
         "environment": {                                    # --- HEX BOARD GAME ---
             "log_level": logging.INFO,                      # Print log messages with this level
-            "size": 7,                                      # Size of the Hex Board
+            "size": 5,                                      # Size of the Hex Board
         },
-        "rl": {                                             # --- REINFORCEMENT LEARNER ---
+        "rl": {                                             # === REINFORCEMENT LEARNER ===
             "visualize": False,                             # Render the board [ True | False ]
             "use_cnn": True,                                # Train with CNN instead of ANN
             "epochs": 1,                                    # Number of epochs on training
             "game_batch": 3,                                # Number of games in each training batch
-            "training_steps": None,                         # Number of training steps (episodes) to run
+            "training_steps": 1000,                         # Number of training steps (episodes) to run
             "epsilon_decay": 0.99,                          # Epsilon decay per training step
             "new_games_per_training_step": 7,               # Regulates when training runs (!)( >game_batch )
             "persist_model_per_training_step": 5,           # Persist model on each X 'th training step
         },
-        "cnn": {                                            # --- CONVOLUTIONAL NEURAL NETWORK ---
-            "learning_rate": 0.001,                         # Alpha
-            "hidden_layers": (64, 64, 64, 64, 64),          # Hidden conv-layers
-            "activation": "relu",                           # Activation function [relu, ...]
-            "optimizer": "adam",                            # Optimizer [adam, ...],
+        "cnn": {                                            # === ARTIFICIAL (CONVOLUTIONAL) NEURAL NETWORK ===
+            "learning_rate": 0.003,                         # Alpha
+            "hidden_layers": (64, 64, 64),                  # Hidden conv-layers
+            "activation": "relu",                           # Activation function [relu | linear | sigmoid | tanh]
+            "optimizer": "adam",                            # Optimizer [adam | adagrad | SGD | RMSProp],
             "reg_const": 0.0001,                            # Kernel regularization "penalty"
             "temperature": 1                                # Temperature used on distribution
         },
-        "ann": {                                            # --- ARTIFICIAL (DENSE) NEURAL NETWORK ---
+        "ann": {                                            # === ARTIFICIAL (DENSE) NEURAL NETWORK ===
             "learning_rate": 0.01,                          # Alpha
             "hidden_layers": (100, 100, 50),                # Hidden dense-layers
             "activation": "relu",                           # Activation function
             "optimizer": "adam",                            # Optimizer
             "temperature": 1                                # Temperature used on distribution
         },
-        "mcts": {                                           # --- MONTE CARLO TREE SEARCH ---
+        "mcts": {                                           # === MONTE CARLO TREE SEARCH ===
             "log_level": logging.INFO,                      # Print log messages with this level
             "use_time_budget": False,                       # Use time budget instead of rollouts
             "time_budget": 0.1,                             # Time budget (in seconds)
-            "searches": 5000,                               # MCTS-searches
+            "searches": 2000,                               # MCTS-searches
             "c": 1.4,                                       # Exploration constant
             "epsilon": 1,                                   # Percentage of random moves in rollout
             "temperature": 0.25                             # Temperature used on distribution
         },
-        "rbuf": {                                           # --- RBUF ---
+        "rbuf": {                                           # === REPLAY BUFFER ===
             "load_samples": False,                          # Initialize samples
             "queue_size": 100,                              # Buffer-size (FiFo)
         },
-        "topp": {                                           # --- TOURNAMENT OF PROGRESSIVE POLICIES ---
-            "visualize": True,                              # Render the board [ True | False]
-            "matches": 50,                                  # Matches between a pair of agents as each player
+        "topp": {                                           # === TOURNAMENT OF PROGRESSIVE POLICIES ===
+            "visualize": False,                             # Render the board [ True | False]
+            "matches": 200,                                 # Matches between a pair of agents as each player
         },
-        "oht": {                                            # --- ONLINE HEX TOURNAMENT ---
+        "oht": {                                            # === ONLINE HEX TOURNAMENT ===
             "qualify": False,                               # Qualify towards subject grading
             "visualize": False,                             # Render the board [ True | False]
-            "mode": "league",                               # Competition mode [ qualifiers | league ]
+            "mode": "qualifiers",                           # Competition mode [ qualifiers | league ]
             "auth": "e1431af64ca24ffa9f2f3887e6b41a32",     # Authentication key
-            "agent": "(1) CNN_S7_B1638.h5",                 # Agent to use in OHT
+            "agent": "7x7/(1) CNN_S7_B1638.h5",             # Agent to use in OHT
         },
     }
 
